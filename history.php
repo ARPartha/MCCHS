ecc8c79a62e7f2dd6d72358189bce3fd8385ad72
AbdurRahmanPartha <abrahmanpartha@gmail.com>
2020-04-20 21:54:06 +0600

    uploading file

---
+++ history.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+    include 'header.php';
+    ?>
+</head>
+<body>
+<body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">Start Bootstrap</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button
+            >
+            <ul class="navbar-nav ml-auto ml-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <div class="dropdown-divider" style="float: right;"></div>
+                        <a class="dropdown-item" href="login.php">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            
+                            <a class="nav-link" href="index.php"
+                                ><div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Request</a>
+                           
+                            <a class="nav-link" href="history.php"
+                                ><div class="sb-nav-link-icon"><i class="fas fa-newspaper"></i></div>
+                                History </a
+                            >
+                            
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+            <div class="tab">
+                <button class="tablinks" onclick="openCity(event, 'Bank')">Bank</button>
+                <button class="tablinks" onclick="openCity(event, 'Bkash')">Bkash</button>
+
+                </div>
+
+                <div id="Bank" class="tabcontent">
+                <h1 class="mt-4">History</h1> 
+                                               
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search By Saving Number" title="Type in a name">
+                                    <table id="bankTable" class="display" style="width:100%">
+                                        <thead>
+                                             <tr>
+                                                <th>Name</th>
+                                                <th>Father's Name</th>
+                                                <th>MobileNo</th>
+                                                <th>DateOfBirth</th>
+                                                <th>Address</th>
+                                                <th>SavingNo</th>
+                                                <th>Account Holder Name</th>
+                                                <th>Account Number</th>
+                                                <th>Bank Name</th>
+                                                <th>Branch Name</th>
+                                                <th>cheque Picture</th>
+                                                <th>Status</th>
+                                             </tr>
+                                        </thead>
+                                        <tbody id="blist">
+                                                            
+                                        </tbody>
+                                                         
+                                    </table>
+                            </div>
+                        </div>
+                </div>
+                <div class="card mb-4">
+                        <Button id="bankexcel">Export Excel</Button>
+                </div>
+                </div>
+
+                <div id="Bkash" class="tabcontent">
+                <h1 class="mt-4">History</h1> 
+                                               
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search By Saving Number" title="Type in a name">
+                                    <table id="bkashTable" class="display" style="width:100%">
+                                        <thead>
+                                             <tr>
+                                                <th>Name</th>
+                                                <th>Father's Name</th>
+                                                <th>MobileNo</th>
+                                                <th>DateOfBirth</th>
+                                                <th>Address</th>
+                                                <th>SavingNo</th>
+                                                <th>Bkash Number</th>
+                                                <th>cheque Picture</th>
+                                                <th>Status</th>
+                                                </tr>
+                                        </thead>
+                                        <tbody id="blist">
+                                                            
+                                        </tbody>
+                                                         
+                                    </table>
+                            </div>
+                        </div>
+                </div> 
+                <div class="card mb-4">
+                        <Button id="bkashexcel">Export Excel</Button>
+                </div>
+                </div>
+                <!-- Tab     -->
+                 
+                    </div>
+                </main>
+               <?php
+                     include 'footer.php';
+               ?>
+            </div>
+                <!--Printing Excel  -->
+                <script type="text/javascript">
+                ///// Bkash 
+                       $('#bkashexcel').click(function(){
+                           XLExport('bkashTable','bkashTable'); 
+                       }); 
+                ///// Bank        
+                       $('#bankexcel').click(function(){
+                           XLExport('bankTable','bankTable'); 
+                       });   
+                    //    function XLExport(tableId){
+                    //     var tab_text = "<table border='2px'><tr bgcolor='#87AFC6'>";
+                    //     var textRange;
+                    //     var j = 0;
+                    //     tab = document.getElementById(tableId);
+                    //     for (j = 0 ; j < tab.rows.length ; j++) {
+                    //         tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
+                    //     }
+                    //     tab_text = tab_text + "</table>";
+                    //     tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+                    //     tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
+                    //     tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // remove input params
+                    //     sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+                    //     return (sa);
+                    //    }
+                        
+                            function XLExport(tableId, filename = ''){
+                                    var downloadurl;
+                                    var dataFileType = 'application/vnd.ms-excel';
+                                    var tableSelect = document.getElementById(tableId);
+                                    var tableHTMLData = tableSelect.outerHTML.replace(/ /g, '%20');
+                                    
+                                    // Specify file name
+                                    filename = filename?filename+'.xls':'export_excel_data.xls';
+                                    
+                                    // Create download link element
+                                    downloadurl = document.createElement("a");
+                                    
+                                    document.body.appendChild(downloadurl);
+                                    
+                                    if(navigator.msSaveOrOpenBlob){
+                                        var blob = new Blob(['\ufeff', tableHTMLData], {
+                                            type: dataFileType
+                                        });
+                                        navigator.msSaveOrOpenBlob( blob, filename);
+                                    }else{
+                                        // Create a link to the file
+                                        downloadurl.href = 'data:' + dataFileType + ', ' + tableHTMLData;
+                                    
+                                        // Setting the file name
+                                        downloadurl.download = filename;
+                                        
+                                        //triggering the function
+                                        downloadurl.click();
+                                    }
+                                }
+                                
+                     
+                </script>
+                <!--Printing Excel  -->
+                <script>
+                function openCity(evt, cityName) {
+                var i, tabcontent, tablinks;
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                document.getElementById(cityName).style.display = "block";
+                evt.currentTarget.className += " active";
+                }
+                </script>
+                <script>
+                    function myFunction() {
+                    var input, filter, table, tr, td,td2, i, txtValue;
+                    input = document.getElementById("myInput");
+                    filter = input.value.toUpperCase();
+                    table = document.getElementById("example");
+                    tr = table.getElementsByTagName("tr");
+                    for (i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td")[5] ;
+                        td2= tr[i].getElementsByTagName("td")[1] ;
+                        if (td ) {
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                        
+                        }
+                        
+                    }
+                }
+                </script>
+                
+</body>
+</html> 
